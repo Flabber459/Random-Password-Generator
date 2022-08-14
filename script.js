126e2567e1d1aab443635d00ed22e0bceac9e43b
Mario Git <marioapalozzo@gmail.com>
2022-08-14 20:38:35 +1000

    Making all parts with random characters

--- script.js
+++ script.js
@@ -18,26 +18,49 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", makePassword);
 
-// Below creates (number amount of) random letters
+// Below creates (number amount of) random capital letters
 
 function makePassword() {
 
   var number = window.prompt("How many characters?");
+  var uppercaseCheck = window.confirm("Do you want uppercase letters?");
+  var lowercaseCheck = window.confirm("Do you want lowercase letters?")
+  var numericCheck = window.confirm("Do you want numeric characters?")
+  var specialCheck = window.confirm("Do you want special characters?")
   var space = "";
 
   for (var i = 0; i < number; i++) {
+    var combined = []
     var uppercase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-    if (number >=8 && number <= 128) {
-    
-    var randomUpper = uppercase[Math.floor(Math.random() * uppercase.length)]
-    space += randomUpper + "";
+    var lowercase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+    var numeric = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+    var special = ["~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "+", "=", "{", "}", "[", "]", "|", "/", "'", "<", ">", ",", ".", "?"]
 
-  document.getElementById("password").innerHTML = space;
-  console.log(space)
+    if (number >=8 && number <= 128) {
+      if (uppercaseCheck) {
+        combined.push.apply(combined, uppercase);
+      }
+      if (lowercaseCheck) {
+        combined.push.apply(combined, lowercase);
+      }
+      if (numericCheck) {
+        combined.push.apply(combined, numeric);
+      }
+      if (specialCheck) {
+        combined.push.apply(combined, special)
+      }
+        var randomGroups = combined[Math.floor(Math.random() * combined.length)]
+        space += randomGroups + "";
+
+        document.getElementById("password").innerHTML = space;
+    }
+
+    else if (number <8 || number > 128) {
+    document.getElementById("password").innerHTML = "Please choose a number between 8 and 128."
     }
   }
 }
-
+// find a way to set minimum value of 1 of each
 
 
 
@@ -54,7 +77,7 @@ var lowercase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p"
 var randomLower = lowercase[Math.floor(Math.random() * lowercase.length)]
 console.log(randomLower);
 
-var numeric = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
+var numeric = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 var randomNumeric = numeric[Math.floor(Math.random() * numeric.length)]
 console.log(randomNumeric);
 
