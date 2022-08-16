23901f2960382733ef4cd3003a56afe5c97c2782
Mario Git <marioapalozzo@gmail.com>
2022-08-17 00:20:54 +1000

    Updating

--- script.js
+++ script.js
@@ -1,40 +1,28 @@
-
 // Assignment code here
-
+var combined = []
+var lowercase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var uppercase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var numeric = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var special = ["~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "+", "=", "{", "}", "[", "]", "|", "/", "'", "<", ">", ",", ".", "?"]
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-//  window.prompt("How many characters?") 
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-  
-  passwordText.value = password;
-  
-}
-
 // Add event listener to generate button
-generateBtn.addEventListener("click", makePassword);
+generateBtn.addEventListener("click", writePassword);
 
-function makePassword() {
+function writePassword() {
 
-  var number = window.prompt("How many characters would you like your password to contain?");
+  var characters = window.prompt("How many characters would you like your password to contain?");
   var lowercaseCheck = window.confirm("Click OK to confirm including lowercase characters.")
   var uppercaseCheck = window.confirm("Click OK to confirm including uppercase characters.");
   var numericCheck = window.confirm("Click OK to confirm including numeric characters.")
   var specialCheck = window.confirm("Click OK to confirm including special characters.")
   var space = "";
 // maybe loop the above: " Click OK to confirm including+ (type) + characters"
-  for (var i = 0; i < number; i++) {
-    var combined = []
-    var lowercase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-    var uppercase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-    var numeric = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-    var special = ["~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "+", "=", "{", "}", "[", "]", "|", "/", "'", "<", ">", ",", ".", "?"]
-
-    if (number >= 8 && number <= 128) {
+  for (var i = 0; i < characters; i++) {
+   
+    if (characters >= 8 && characters <= 128) {
       if (lowercaseCheck) {
         combined.push.apply(combined, lowercase);
       }
@@ -52,9 +40,10 @@ function makePassword() {
 
       document.getElementById("password").innerHTML = space;
       }
-      else if (number < 8 || number > 128) {
+      else if (characters < 8 || characters > 128) {
     document.getElementById("password").innerHTML = "Please choose a number between 8 and 128."
       }
+      
     }
     if (
         lowercaseCheck === false &&
@@ -66,3 +55,7 @@ function makePassword() {
   }
 
 // find a way to set minimum value of 1 of each
+// When I don't add a value nothing happens
+// Nothing happens when you write a non-number
+
+// add comments
