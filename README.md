df1d5bb9f78c3f681f90d469b0ec82107067b7b0
Mario Git <marioapalozzo@gmail.com>
2022-08-18 00:20:25 +1000

    Further changes

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# Module-3-Challenge-MP
+# Random Password Generator
+
+## Description
+
+I undertook this project in order to help users create a randomly generated password, based on parameters that the user sets. In a world where passwords are required for many different activities in daily life, this project allows its users to provide themselves with secure passwords that are not going to be easily guessed.
+
+The creation of this project allowed me to gain experience with different aspects and uses of javascript, including the use of randomised generators, for loops, if statements and functions. It allowed me to better grasp the fundamentals of coding logic and to apply this to creating a useful program.
+
+## Usage
+
+The individual is to click on the part that says 'Generate Password', where they will then be directed to answer a series of prompts that ask them to enter a password length, as well as whether or not they want lowercase, uppercase, numeric and/or special characters in their password. This then displays their randomly generated password.
+
+If the user does not input an adequate value or does not select any type of characters to include in their password, they are asked to follow the prompts properly before a password is able to be provided to them.
+
--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@ var combined = []
 var lowercase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 var uppercase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 var numeric = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-var special = ["~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "+", "=", "{", "}", "[", "]", "|", "/", "'", "<", ">", ",", ".", "?"]
+var special = [":", ";","^", "~", '"', "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "+", "=", "{", "}", "[", "]", "|", "/", "'", "<", ">", ",", ".", "?"];
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -13,48 +13,51 @@ generateBtn.addEventListener("click", writePassword);
 
 function writePassword() {
   
-  var characters = parseInt(
-  prompt("How many characters would you like your password to contain?"))
-  var lowercaseCheck = confirm("Click OK to confirm including lowercase characters.")
-  var uppercaseCheck = confirm("Click OK to confirm including uppercase characters.");
-  var numericCheck = confirm("Click OK to confirm including numeric characters.")
-  var specialCheck = confirm("Click OK to confirm including special characters.")
-  var password = "";
-
-  if (characters !== NaN) { // If user inputs a number
-for (var i = 0; i < characters; i++) {
+  var characters = 
+  prompt("How many characters would you like your password to contain?");
   
-  if (characters >= 8 && characters <= 128) {
-    if (lowercaseCheck) { // If user confirms they want lowercase characters, push the relevant array into 'combined' array
-        combined.push.apply(combined, lowercase);
-    }
-    if (uppercaseCheck) { // If user confirms uppercase, push array into 'combined' array
-        combined.push.apply(combined, uppercase);
-    }
-    if (numericCheck) { // If user confirms numbers, push array into 'combined' array
-        combined.push.apply(combined, numeric);
-    }
-    if (specialCheck) { // If user confirms special characters, push array into 'combined' array
-        combined.push.apply(combined, special)
-    }
-    var random = combined[Math.floor(Math.random() * combined.length)] // Creates random password using combined array (confirmed values)
-    password += random + "";
-
-    document.getElementById("password").innerHTML = password; // puts password into
-      }
-  
-      else if (characters < 8 || characters > 128) {
+  if (isNaN(characters)) { // if user did not type a number
     document.getElementById("password").innerHTML = "Please choose a number between 8 and 128."
+    return;
+  } else if (characters < 8 || characters > 128) {
+    document.getElementById("password").innerHTML = "Please choose a number between 8 and 128."
+    return;
+  } else { // If user inputs a number with a correct value
+
+  var lowercaseCheck = confirm("Click OK to confirm including lowercase characters.");
+  var uppercaseCheck = confirm("Click OK to confirm including uppercase characters.");
+  var numericCheck = confirm("Click OK to confirm including numeric characters.");
+  var specialCheck = confirm("Click OK to confirm including special characters.");
+  var password = "";
+  
+    for (var i = 0; i < characters; i++) {
+  
+      if (characters >= 8 && characters <= 128) {
+        // If user confirms they want lowercase characters, push the relevant array into 'combined' array
+        if (lowercaseCheck) { 
+        combined.push.apply(combined, lowercase);
+        } // If user confirms uppercase, push array into 'combined'
+        if (uppercaseCheck) { 
+        combined.push.apply(combined, uppercase);
+        } // If user confirms numbers, push array into 'combined'
+        if (numericCheck) { 
+        combined.push.apply(combined, numeric);
+        } // If user confirms special characters, push array into 'combined'
+        if (specialCheck) { 
+        combined.push.apply(combined, special);
+        } // Creates random password using combined array (confirmed values)
+        var random = combined[Math.floor(Math.random() * combined.length)];
+        password += random + "";
+
+        document.getElementById("password").innerHTML = password; // puts password into textbox
       }
     }
-      if ( // If user did not select any character type
-        lowercaseCheck === false &&
-        uppercaseCheck === false &&
-        numericCheck === false &&
-        specialCheck === false) {
+    // If user did not select any character type, puts the following message instead
+    if (lowercaseCheck === false &&
+      uppercaseCheck === false &&
+      numericCheck === false &&
+      specialCheck === false) { 
       document.getElementById("password").innerHTML = "Please choose at least one type of character to include in your password."
     } 
-  } if (isNaN(characters)) { // if user did not type a number
-  document.getElementById("password").innerHTML = "Please choose a number between 8 and 128."
   }
 }
